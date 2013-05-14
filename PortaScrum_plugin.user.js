a11bb1ecb6fb43c26a84631c1b62889e5e865667
quanlinc <quanlinc@gmail.com>
2013-05-13 23:58:09 -0700

    adding some function interface, comments and psudo code

--- PortaScrum_plugin.user.js
+++ PortaScrum_plugin.user.js
@@ -23,6 +23,7 @@ var plugin = (function($, doc){
     };
     
     //div id which contains the information of a story
+    //copy the info inside a story, get ready for posting
     var copy = function(story){
         var storyName=$('#' + story.attr("id") + '> h3 > span[id^="story_description"]').text();
         var tasks = new Array();
@@ -33,6 +34,8 @@ var plugin = (function($, doc){
         });
     };
     
+    //construct a data structure that will be passed to post data agaist next sprint
+    //TODO:need to think about what to pass in and how to put into data structure
     var constructData = function(storyName, tasks, owner){
         var story = {
             "storyName": storyName,
@@ -41,13 +44,66 @@ var plugin = (function($, doc){
         for(var i in tasks){
             var task = tasks[i];
             story.tasks.push({
-                "task"+i: task,
+                "task": task,
                 "owner": owner
+		"estimate":estimate
             });
         }
         
         return story;
     }
+
+    var postData = function(storyName, tasks){
+	//TODO: 1.find the next sprint url
+	//      2.post against that url, creat story first
+	//      3.locate the new story that has been created, adding new tasks to it
+	var targetURL;
+	
+	if(hasNextSrint()){
+	    //route to sprint list page, find the url of last sprint
+	} else {//TODO: probably popover a input dialogue to get new sprint name
+	    targetURL = createNewSprint(sprintName);
+	}
+
+	$.ajax(
+	    url: targetURL,
+	    type: "POST",
+	    data: storyName,
+	    success:function(html){//successfully created new story
+		//add new tasks
+		var newStoryLocation = $(html).find('');
+		//might need to do $.each for tasks
+		$.ajax(
+		    url: targetURL,
+		    type: "POST",
+		    data: tasks,
+		    success:function(html){
+
+		    },
+		    error: function(XMLHttpRequest, textStatus, errorThrown){
+
+		    }
+		);
+	    },
+	    error: function(XMLHttpRequest, textStatus, errorThrown){
+
+	    }
+	);
+    };
+
+    //verify if we have a new sprint to copy our story over
+    //@return yes or no
+    var hasNextSprint = function(){
+
+    };
+    
+    //If we don't have a sprint to copy our data to, create a new one.
+    //@return url of the new sprint
+    var createNewSprint = function(sprintName){
+	return url;
+    };
+
+    
 
 })(jQuery, document)
 

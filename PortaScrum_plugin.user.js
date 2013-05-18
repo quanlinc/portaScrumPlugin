c236f117b1cc359fab89fbb7b41483698aecada4
jeff <jeffxenog@qq.com>
2013-05-18 23:44:06 +0800

    优化jquery使用以及解决语法错误

--- PortaScrum_plugin.user.js
+++ PortaScrum_plugin.user.js
@@ -1,111 +1,119 @@
-var plugin = (function($, doc){
+var plugin = (function ($, doc) {
 
-    var init = function(){
-      //jQuery(document).ready(function(){alert("hello");})
-      initUI();
+    var init = function () {
+        //jQuery(document).ready(function(){alert("hello");})
+        initUI();
     };
-    
-    var initUI = function(){
-      $('img[alt="Back"]').each(function(){
-        var backlogLink = $(this).parent();
-        $(backlogLink).before('<input type="button" value="copy"/>');
-      });
+
+    var initUI = function () {
+        $('img[alt="Back"]').each(function () {
+            var backlogLink = $(this).parent();
+            $(backlogLink).before('<input type="button" value="copy"/>');
+        });
     };
 
     return {
-        init:init
+        init: init
     }
-    
-    var initListener = function(){
-        $('input[value="copy"]').click(function(e){
+
+    var initListener = function () {
+        $('input[value="copy"]').click(function (e) {
             var targetId = e.target.id;
         });
     };
-    
+
     //div id which contains the information of a story
     //copy the info inside a story, get ready for posting
-    var copy = function(story){
-        var storyName=$('#' + story.attr("id") + '> h3 > span[id^="story_description"]').text();
+    var copy = function (story) {
+        //var storyName=$('#' + story.attr("id") + '> h3 > span[id^="story_description"]').text();
+        var storyName = $('#' + story.attr("id")).find('>h3').find('>span[id^="story_description"]').text();
+        //如果你的story是jquery对象应该可以用下面这种写法
+        //story.find('>h3').find('>span[id^="story_description"]').text();
+
+
         var tasks = new Array();
-        
-        $(story).find('table').find('tr[class$="taskInProgress"]').each(function(){
+
+        $(story).find('table').find('tr[class$="taskInProgress"]').each(function () {
             var taskName = $(this).find('span').text();
             var owner = $(this).find('td[class^="worker"]').text();
         });
     };
-    
+
     //construct a data structure that will be passed to post data agaist next sprint
     //TODO:need to think about what to pass in and how to put into data structure
-    var constructData = function(storyName, tasks, owner){
+    var constructData = function (storyName, tasks, owner) {
         var story = {
             "storyName": storyName,
             "tasks": []
         };
-        for(var i in tasks){
+        for (var i in tasks) {
             var task = tasks[i];
             story.tasks.push({
                 "task": task,
-                "owner": owner
-		"estimate":estimate
+                "owner": owner,
+                "estimate": estimate
             });
         }
-        
+
         return story;
     }
 
-    var postData = function(storyName, tasks){
-	//TODO: 1.find the next sprint url
-	//      2.post against that url, creat story first
-	//      3.locate the new story that has been created, adding new tasks to it
-	var targetURL;
-	
-	if(hasNextSrint()){
-	    //route to sprint list page, find the url of last sprint
-	} else {//TODO: probably popover a input dialogue to get new sprint name
-	    targetURL = createNewSprint(sprintName);
-	}
+    var postData = function (storyName, tasks) {
+        //TODO: 1.find the next sprint url
+        //      2.post against that url, creat story first
+        //      3.locate the new story that has been created, adding new tasks to it
+        var targetURL;
 
-	$.ajax(
-	    url: targetURL,
-	    type: "POST",
-	    data: storyName,
-	    success:function(html){//successfully created new story
-		//add new tasks
-		var newStoryLocation = $(html).find('');
-		//might need to do $.each for tasks
-		$.ajax(
-		    url: targetURL,
-		    type: "POST",
-		    data: tasks,
-		    success:function(html){
+        if (hasNextSrint()) {
+            //route to sprint list page, find the url of last sprint
+        } else {//TODO: probably popover a input dialogue to get new sprint name
+            targetURL = createNewSprint(sprintName);
+        }
 
-		    },
-		    error: function(XMLHttpRequest, textStatus, errorThrown){
+        $.ajax({
+                url: targetURL,
+                type: "POST",
+                data: storyName,
+                success: function (html) {//successfully created new story
+                    //add new tasks
+                    var newStoryLocation = $(html).find('');
+                    //might need to do $.each for tasks
+                    $.ajax({
+                            url: targetURL,
+                            type: "POST",
+                            data: tasks,
+                            success: function (html) {
 
-		    }
-		);
-	    },
-	    error: function(XMLHttpRequest, textStatus, errorThrown){
+                            },
+                            error: function (XMLHttpRequest, textStatus, errorThrown) {
 
-	    }
-	);
+                            }
+                        }
+
+                    );
+                },
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+
+                }
+            }
+
+        );
     };
 
     //verify if we have a new sprint to copy our story over
     //@return yes or no
-    var hasNextSprint = function(){
+    var hasNextSprint = function () {
 
     };
-    
+
     //If we don't have a sprint to copy our data to, create a new one.
     //@return url of the new sprint
-    var createNewSprint = function(sprintName){
-	return url;
+    var createNewSprint = function (sprintName) {
+        return url;
     };
 
-    
 
-})(jQuery, document)
+})(jQuery, document);
 
 
 plugin.init();
